4b859e1add8170f38964e2b1c698680f18464955
秦 道平 <reynoldqin@gmail.com>
2014-02-15 21:56:43 +0800

    first commit

---
+++ WKInputAccessoryView/WKInputAccessoryView/WKInputAccessoryView.h
@@ -0,0 +1,13 @@
+//
+//  WKInputAccessoryView.h
+//  WKInputAccessoryView
+//
+//  Created by 秦 道平 on 14-2-15.
+//  Copyright (c) 2014年 秦 道平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKInputAccessoryView : UIView
+
+@end
---
+++ WKInputAccessoryView/WKInputAccessoryView/WKInputAccessoryView.m
@@ -0,0 +1,31 @@
+//
+//  WKInputAccessoryView.m
+//  WKInputAccessoryView
+//
+//  Created by 秦 道平 on 14-2-15.
+//  Copyright (c) 2014年 秦 道平. All rights reserved.
+//
+
+#import "WKInputAccessoryView.h"
+
+@implementation WKInputAccessoryView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
